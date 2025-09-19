ec5b718f7517e658e9c2fe9fb50b3da0c21e2e21
Geoffred Roe Dizon <geoffred.dizon@cit.edu>
2025-09-19 13:28:51 +0800

    Implementing add

--- src/boardarray.h
+++ src/boardarray.h
@@ -16,6 +16,15 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+            for(int i = 0; i < index; i++){
+                if(array[i].compare(entry)){
+                    for(int i = i + 1; i < index; i++){
+                        array[i] = array[i-1];
+
+                    }
+                    array[i] = *entry;
+                }
+            }
             return;
         }
 
